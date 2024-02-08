6a3c12036604e4410675ade08ea0c6d6035e28b7
Shaun Knowles <shaun@primitivedigital.uk>
2024-02-08 17:02:02 +0000

    add image size - via theme for now

--- functions.php
+++ functions.php
@@ -23,11 +23,6 @@ Description: Register custom image sizes for use in your WordPress site.
 // Register custom image size
 // Add custom image size used in Cover Template.
 function custom_image_sizes() {
-    add_image_size( 'bedrock-fullscreen', 1980, 9999, true ); // Change dimensions as needed
+    add_image_size( 'bedrock-custom', 1980, 9999 ); // Change dimensions as needed, add true to crop.
 }
 add_action( 'after_setup_theme', 'custom_image_sizes' );
-
-
-
-// Add custom image size used in Cover Template.
-//	add_image_size( 'twentytwenty-fullscreen', 1980, 9999 );
